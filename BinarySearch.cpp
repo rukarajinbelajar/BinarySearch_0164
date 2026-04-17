6c3e4bede4e5bb52b6d8f91972292cbadff7b98b
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-17 15:11:40 +0700

    menampilkan isi array pada index j

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -58,4 +58,8 @@ void display()
     cout << "\n====================================\n";
 
     for (int j = 0; j < nPanjang; j++)
+    {
+        cout << element[j];
+        if (j < nPanjang - 1)
+    }
 }
