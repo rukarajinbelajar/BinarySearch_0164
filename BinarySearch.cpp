6521a28cc951917d6df1acfaadcf6e5919bce1f0
rukarajinbelajar <maar1101dnd@gmail.com>
2026-04-17 14:59:47 +0700

    perulangan input array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -26,3 +26,7 @@ void input()
 cout << "\n====================================\n";
 cout << "       Masukkan Elemen Array          \n";
 cout << "\n====================================\n";
+
+for (int i = 0; i < nPanjang; i++)
+    {
+        cout << "Data ke-" << (i + 1) << "  = ";
